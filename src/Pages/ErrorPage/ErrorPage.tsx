eb128b8e7c408044d67e9d8efb519239002581b2
User Nika  Azizova <nikaazizova64@gmail.com>
2025-07-09 17:08:20 +0400

    feat: add ErrorPage

--- src/Pages/ErrorPage/ErrorPage.tsx
+++ src/Pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,15 @@
-export default function ErrorPage () {
-    return(
-        <div>ErrorPage</div>
+import styles from './ErrorPage.module.css';
+
+export default function ErrorPage() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.subWrapper}>
+                    <div className={styles.title}>404</div>
+                    <div className={styles.text}>Oops...page not found</div>
+                    <div className={styles.subtext}>We don't know how you ended up here, but you should go away now.</div>
+                </div>
+            </div>
+        </div>
     )
 }
